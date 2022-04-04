da4382f5e747de499f5a2a4ef44dbbe956f2e687
tofusupreme <yunishbacon@gmail.com>
2022-04-04 10:24:11 +0900

    Updated App.js, added ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
   return (
     <div>
       <h2> Whoo hoo! Finally getting things started! </h2>
       <p> Welcome to React </p>
+      <ExpenseItem />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
 function ExpenseItem() {
   return <h2> Expense Item </h2>
 }
+export default ExpenseItem

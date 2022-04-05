8c27ffcc624eacd6bd6a4d17a68af48a3747a425
tofusupreme <yunishbacon@gmail.com>
2022-04-05 13:17:11 +0900

    passed new Expenses prop to App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Expenses from './components/Expenses';
 
 function App() {
-  const items = [
+  const expenses = [
     {
       id: 'e1',
       title: 'Grocery',
@@ -31,22 +31,7 @@ function App() {
   return (
     <div>
       <h2> The Expense Chart</h2>
-      <Expenses
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date} />
-      <Expenses
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date} />
-      <Expenses
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date} />
-      <Expenses
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date} />
+      <Expenses items={expenses} />
     </div>
   );
 }

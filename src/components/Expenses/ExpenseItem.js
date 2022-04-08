9eea9f40bdb4bb5239bac964225ac6b2aae36d02
tofusupreme <yunishbacon@gmail.com>
2022-04-08 22:20:30 +0900

    Added clickHandler event listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,11 @@ import Card from '../UI/Card';
 
 
 function ExpenseItem(props) {
+
+  const clickHandler = () => {
+    console.log('clicked');
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
@@ -11,7 +16,7 @@ function ExpenseItem(props) {
         <h2> {props.title} </h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
-      <button>Change Title</button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }

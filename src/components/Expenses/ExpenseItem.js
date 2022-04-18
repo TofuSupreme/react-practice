c1d666c18a1b98590c0a9a1406c12a5e778ed82b
tofusupreme <yunishbacon@gmail.com>
2022-04-18 10:06:15 +0900

    Added state for setTitle

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,11 +5,11 @@ import Card from '../UI/Card';
 
 
 const ExpenseItem = (props) => {
-  const [] = useState(props.title);
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
+
 
   const clickHandler = () => {
-    title = 'Clicked';
+    setTitle('Updated');
     console.log(title)
   }
 
@@ -17,7 +17,7 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2> {props.title} </h2>
+        <h2> {title} </h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>

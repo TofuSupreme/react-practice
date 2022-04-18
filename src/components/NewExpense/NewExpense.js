24f58323455a723aa6f87606a7b68d6006072e3e
tofusupreme <yunishbacon@gmail.com>
2022-04-18 14:00:05 +0900

    Created NewExpense style sheet

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NewExpense.css';
+const newExpense = () => {
+  return (
+    <div className="new-expense">
+      <form></form>
+    </div>
+  )
+};
+
+export default newExpense

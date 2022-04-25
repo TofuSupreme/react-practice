6cdc13660a5990c232c03ed32c08e85b64ea283c
tofusupreme <yunishbacon@gmail.com>
2022-04-25 21:08:14 +0900

    Two-way binding in forms

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -51,15 +51,24 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler }/>
+          <input type="text"
+            value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" onChange={amountChangeHandler} min="0.01" step="0.01"/>
+          <input type="number"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+            min="0.01"
+            step="0.01" />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" onChange={dateChangeHandler} min="1970-07-07" max="2050-05-05"/>
+          <input type="date"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+            min="1970-07-07"
+            max="2050-05-05" />
         </div>
         <div className="new-expense__actions">
           <button type="submit">Add Expense</button>
